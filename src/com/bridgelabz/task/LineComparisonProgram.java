c637e4a570f7733ebbed7f21a1143d1b0c90582f
Qaiser789 <sutarqaiser@gmail.com>
2024-01-27 10:04:16 +0530

    Welcome message added.

---
+++ src/com/bridgelabz/task/LineComparisonProgram.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.task;
+
+public class LineComparisonProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Line Comparison Computation Program");
+
+	}
+
+}
